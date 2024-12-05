c635556e03ab82dc517a812447dcd1862be27782
Janelle2002 <bandohjanelle@gmail.com>
2024-12-05 14:28:53 -0500

    Initial commit

---
+++ src/components/canvas.js
@@ -0,0 +1,18 @@
+// src/components/canvas.js
+
+export const EarthCanvas = () => {
+    // EarthCanvas component code
+  };
+  
+  export const BallCanvas = () => {
+    // BallCanvas component code
+  };
+  
+  export const ComputersCanvas = () => {
+    // ComputersCanvas component code
+  };
+  
+  export const StarsCanvas = () => {
+    // StarsCanvas component code
+  };
+  
